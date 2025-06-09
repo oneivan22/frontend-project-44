eed59cfc3f191ce2c4397d16763c0b0f596748ba
One_Ivan <ivanuradnikov439@gmail.com>
2025-06-09 18:39:37 +0300

    Update brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import welcomeUser from '../src/cli.js';
+import welcomeUser from '../src/cli.js'
 
-console.log('Welcome to the Brain Games!');
-const userName = welcomeUser();
-console.log(`Hello, ${userName}!`);
+console.log('Welcome to the Brain Games!')
+const userName = welcomeUser()
+console.log(`Hello, ${userName}!`)
