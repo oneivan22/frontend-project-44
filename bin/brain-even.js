5db290b2cd8d909e9e896e36abe33242551d0aed
One_Ivan <ivanuradnikov439@gmail.com>
2025-06-09 18:37:22 +0300

    Update brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
-import { isEven, generationNumber } from '../src/helpers/helpers.js';
-import runGame from '../src/runGame.js';
+import { isEven, generationNumber } from '../src/helpers/helpers.js'
+import runGame from '../src/runGame.js'
 
 function generateEvenQuestion() {
-  const number = generationNumber();
-  const correctAnswer = isEven(number) ? 'yes' : 'no';
-  const question = String(number);
+  const number = generationNumber()
+  const correctAnswer = isEven(number) ? 'yes' : 'no'
+  const question = String(number)
 
-  return [question, correctAnswer];
+  return [question, correctAnswer]
 }
 
-const description = 'Answer "yes" if the number is even, otherwise answer "no".';
-runGame(description, generateEvenQuestion);
+const description = 'Answer "yes" if the number is even, otherwise answer "no".'
+runGame(description, generateEvenQuestion)
