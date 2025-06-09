72f43cc7e1a9e82653ec3877bbffc7265ab11db4
One_Ivan <ivanuradnikov439@gmail.com>
2025-06-09 18:36:30 +0300

    Update brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 
-import runGame from '../src/runGame.js';
-import { generationMathOperator, calcNums, generationNumber } from '../src/helpers/helpers.js';
+import runGame from '../src/runGame.js'
+import { generationMathOperator, calcNums, generationNumber } from '../src/helpers/helpers.js'
 
 function generateCalcQuestion() {
-  const firstNumber = generationNumber();
-  const secondNumber = generationNumber();
-  const operator = generationMathOperator();
+  const firstNumber = generationNumber()
+  const secondNumber = generationNumber()
+  const operator = generationMathOperator()
 
-  const question = `${firstNumber} ${operator} ${secondNumber}`;
-  const correctAnswer = String(calcNums(firstNumber, secondNumber, operator));
+  const question = `${firstNumber} ${operator} ${secondNumber}`
+  const correctAnswer = String(calcNums(firstNumber, secondNumber, operator))
 
-  return [question, correctAnswer];
+  return [question, correctAnswer]
 }
 
-const description = 'What is the result of the expression?';
-runGame(description, generateCalcQuestion);
+const description = 'What is the result of the expression?'
+runGame(description, generateCalcQuestion)
