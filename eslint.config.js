4921a77baaaa7a53b37d5240a796bebfc838df13
One_Ivan <ivanuradnikov439@gmail.com>
2025-06-09 16:12:36 +0300

    Add files via upload

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,25 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-import stylistic from '@stylistic/eslint-plugin';
-
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([
   stylistic.configs.recommended,
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
-]);
+  { 
+    files: ['**/*.{js,mjs,cjs}'], 
+    plugins: { 
+      '@stylistic': stylistic 
+    },
+    rules: {
+      '@stylistic/semi': ['error', 'never'],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/arrow-parens': ['error', 'as-needed']
+    }
+  },
+  { 
+    files: ['**/*.{js,mjs,cjs}'], 
+    languageOptions: { 
+      globals: globals.node 
+    } 
+  }
+])
