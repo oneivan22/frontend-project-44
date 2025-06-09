3f2257a10bacb2b258b606f23c48788dfb121360
One_Ivan <ivanuradnikov439@gmail.com>
2025-06-09 18:40:42 +0300

    Update brain-progression.js

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
-import { generationArithmeticProgression, replaceRandomWithDots } from '../src/helpers/helpers.js';
-import runGame from '../src/runGame.js';
+import { generationArithmeticProgression, replaceRandomWithDots } from '../src/helpers/helpers.js'
+import runGame from '../src/runGame.js'
 
 function generateProgressionQuestion() {
-  const progression = generationArithmeticProgression();
-  const [changedProgression, hiddenNumber] = replaceRandomWithDots(progression);
-  const question = changedProgression.join(' ');
+  const progression = generationArithmeticProgression()
+  const [changedProgression, hiddenNumber] = replaceRandomWithDots(progression)
+  const question = changedProgression.join(' ')
 
-  return [question, String(hiddenNumber)];
+  return [question, String(hiddenNumber)]
 }
 
-const description = 'What number is missing in the progression?';
-runGame(description, generateProgressionQuestion);
+const description = 'What number is missing in the progression?'
+runGame(description, generateProgressionQuestion)
